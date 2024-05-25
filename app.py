536aab9814616a47bffb1ee02abe62f3c9bbfe56
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-05-24 20:56:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -109,8 +109,9 @@ if uploaded_file is not None and api_key and analyze_button:
                 ],
               max_tokens=300,
               )
-            if response.choices[0].content is not None:
-                    full_response += completion.choices[0].content
+            #response.choices[0].message.content
+            if response.choices[0].mesage.content is not None:
+                    full_response += completion.choices[0].mesage.content
                     message_placeholder.markdown(full_response + "▌")
             # Final update to placeholder after the stream ends
             message_placeholder.markdown(full_response)
